461517d5720f2af56fd1b44b405adf71046f1eb8
Nik92152 <extend52@yandex.ru>
2023-06-30 01:35:52 +0300

    Поставил ссылку бэка

--- client-mern-blog/src/utils/axios.js
+++ client-mern-blog/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'http://fullstack-front.vercel.app',
+    baseURL: 'http://fullstack-six-orcin.vercel.app',
 })
 
 instance.interceptors.request.use((config) => {
