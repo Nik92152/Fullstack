d727c32474c3ff27ffe9283c80e70da4f52e9e61
Nik92152 <extend52@yandex.ru>
2023-06-30 10:12:02 +0300

    1

--- client-mern-blog/src/utils/axios.js
+++ client-mern-blog/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'http://fullstack-six-orcin.vercel.app',
+    baseURL: 'fullstack-all-server.vercel.app',
 })
 
 instance.interceptors.request.use((config) => {
